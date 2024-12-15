3969c3b24f2782600ccb77000b8b2e56b39e39a3
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2024-12-15 12:39:35 +0530

    Update bot.py
    from sample_info import tempDict

--- bot.py
+++ bot.py
@@ -28,6 +28,8 @@ from plugins.webcode import bot_run
 from os import environ
 from aiohttp import web as webserver
 
+from sample_info import tempDict
+
 PORT_CODE = environ.get("PORT", "8080")
 
 class Bot(Client):
