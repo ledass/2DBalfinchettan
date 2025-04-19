62b2128e1b8628e29ac63d1f1c06833295184ca1
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2025-04-19 17:17:20 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -2,21 +2,18 @@ class script(object):
     START_TXT = """𝙷𝙴𝙻𝙾 {},
 Ask Here Movies in Pm 🔪
 𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍
-**Using 2DB Feature**"""
+<b>Using 2DB Feature</b>"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
-    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/im_goutham_josh>Click Here</a>
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: Koyeb
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v2 [ 𝙱𝙴𝚃𝙰 ]"""
+    ABOUT_TXT = """✯ MY NAME: {}
+✯ CREATOR: <a href=https://t.me/im_goutham_josh>Click Here</a>
+✯ LIBRARY: PYROGRAM
+✯ LANGUAGE: PYTHON 3
+✯ DATA BASE: MONGO DB
+✯ BOT SERVER: Koyeb
+✯ BUILD STATUS: v2 [ BETA ]"""
     SOURCE_TXT = """<b>NOTE:</b>
-Editing the Bot 💐
-
-<b>DEVS:</b>
-- <a href=https://t.me/wudixh12>Update Channel :)</a>"""
+Nothing Here"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and bot will respond whenever a keyword is found the message
