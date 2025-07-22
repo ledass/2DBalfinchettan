a5e60de3a71f3b1c62c7c652481c62e4cd602664
ledass <nistalieocs@gmail.com>
2025-07-22 17:30:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -63,7 +63,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}]-🎬-{file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"[{get_size(file.file_size)}]-🔻-{file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
@@ -693,7 +693,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}]-🎬-{file.file_name}",
+                    text=f"[{get_size(file.file_size)}]-🔻-{file.file_name}",
                     callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
